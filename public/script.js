60340e1089e79921c8ab3bfe9dcab6bab39739b0
sanji0219 <81353953+sanji0219@users.noreply.github.com>
2022-06-05 12:21:55 +0900

    m

--- public/script.js
+++ public/script.js
@@ -3,6 +3,5 @@ $(()=>{
     var height=window.innerHeight
     $(".App").css("margin-left",width/2-height*0.295)
     $(".plus").css("right",width/2-height*0.295+20)
-    $(".chakra-input").css("width",height*0.58)
     // $(".Article").css({"width":height*0.59*0.95,"padding-left":height*0.59*0.025,"padding-right":height*0.59*0.025})
 })
