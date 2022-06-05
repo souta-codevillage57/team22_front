6cd56e39a501bf531045ad4aa243dff231945ed7
sanji0219 <81353953+sanji0219@users.noreply.github.com>
2022-06-05 12:27:51 +0900

    m

--- public/script.js
+++ public/script.js
@@ -3,5 +3,4 @@ $(()=>{
     var height=window.innerHeight
     $(".App").css("margin-left",width/2-height*0.295)
     $(".plus").css("right",width/2-height*0.295+20)
-    // $(".Article").css({"width":height*0.59*0.95,"padding-left":height*0.59*0.025,"padding-right":height*0.59*0.025})
 })
